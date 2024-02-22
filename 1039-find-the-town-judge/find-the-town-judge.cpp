3e507bdd9a03ff31c90622b03bd4f95541354416
Karthikkadimi1234 <97425674+Karthikkadimi1234@users.noreply.github.com>
2024-02-22 17:39:49 +0530

    Time: 144 ms (51.56%) | Memory: 67.7 MB (46.10%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        map<int,int>mp;
+        if(n==1 and trust.size()==0) return 1;
+        if(n>1 and trust.size()==0) return -1;
+        for(int i=0;i<trust.size();i++)
+        {
+            mp[trust[i][1]]++;
+        }
+        int ma=INT_MIN,var;
+        for(auto it:mp) 
+        {
+            if(it.second>ma)
+            {
+                ma=it.second;
+                var=it.first;
+            }    
+        }
+        cout<<var<<" ";
+        vector<int>res;
+        for(int i=0;i<trust.size();i++)
+        {
+            res.push_back(trust[i][0]);
+        }
+        if(find(res.begin(),res.end(),var)==res.end() and ma==n-1) return var;
+        return -1;
+    }
+};
