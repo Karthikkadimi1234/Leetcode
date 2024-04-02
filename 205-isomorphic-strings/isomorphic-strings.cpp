de3caca848f1bd59933a670bd5e9915ac3d91f29
Karthikkadimi1234 <97425674+Karthikkadimi1234@users.noreply.github.com>
2024-04-02 22:46:03 +0530

    Time: 8 ms (40.48%) | Memory: 9.3 MB (63.23%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -5,28 +5,20 @@ public:
         return false;
 
     unordered_map<char, char>mp;
-    unordered_set<char> mpc ;
-
     for (int i = 0; i < s.length(); ++i) {
         if (mp.find(s[i]) != mp.end()) {
             if (mp[s[i]] != t[i])
                 return 0;
         } else {
-            if (mpc.find(t[i]) != mpc.end())
-                return 0;
             mp[s[i]] = t[i];
         }
     }
     unordered_map<char, char>mpp;
-    unordered_set<char> mpcp ;
-
     for (int i = 0; i < t.length(); ++i) {
         if (mpp.find(t[i]) != mpp.end()) {
             if (mpp[t[i]] != s[i])
                 return 0;
         } else {
-            if (mpcp.find(s[i]) != mpcp.end())
-                return 0;
             mpp[t[i]] = s[i];
         }
     }
